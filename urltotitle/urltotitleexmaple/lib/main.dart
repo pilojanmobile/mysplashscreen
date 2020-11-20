628c3abd21e26e8357f7f8d70d38df9a63f043a3
Pilojanmobile <pilojan992@gmail.com>
2020-11-20 14:07:36 +0530

    Example add:

--- urltotitle/urltotitleexmaple/lib/main.dart
+++ urltotitle/urltotitleexmaple/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:urltotitle/urltotitle.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,17 +27,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- Urltotitle urltotitle = new Urltotitle(url);
+  String url = "https://www.newssite.com/news/newsoftheday/2021/3/20";
+
+  List<String> urls = [
+    "https://www.newssite.com/news/newsoftheday/2020/3/09",
+    "https://www.newssite.com/news/newsoftheday/2020/3/04",
+    "https://www.newssite.com/news/newsoftheday/2021/3/20"
+  ];
+
+  Urltotitle urltotitle;
 
   @override
   void initState() {
+    urltotitle = new Urltotitle(url);
     gettitle();
     super.initState();
   }
 
-gettitle(){
-  
-}
+  void gettitle() async {
+    Urltotitle urltotitle = new Urltotitle(url);
+
+    // This is get a single url title and body
+    var title = await urltotitle.getTitle();
+    var body = await urltotitle.getbody();
+
+    // This is get a Multiple url title and body
+
+    List<String> listtiles = await urltotitle.getListofTitles(urls);
+    List<String> listbody = await urltotitle.getListofBody(urls);
+
+//print all values
+
+    print("Title :: " + title);
+    print("Body :: " + body);
+    print("listoftitle " + listtiles.toString());
+    print("listofbody " + listbody.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
